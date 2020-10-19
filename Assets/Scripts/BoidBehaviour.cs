c4151b2a4787ac95e6c79966a571c5c395f1568e
Ian <barrowian1991@gmail.com>
2020-10-19 13:30:33 +0100

    Added boids project

---
+++ Assets/Scripts/BoidBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Boids;
+using UnityEngine;
+
+public abstract class BoidBehaviour : ScriptableObject
+{
+    //Inherited by the scriptable object types, all found in BehaviourScript folder
+    public abstract Vector3 CalculateMove(Boid boid, List<Transform> neighbours, BoidLeader leader);
+}
